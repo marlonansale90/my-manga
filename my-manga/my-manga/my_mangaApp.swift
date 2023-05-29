b6b01c529040b326efd74858e273f5ae1c2b39d7
marlon ansale <marlonvon.ansale@fanduel.com>
2023-05-29 16:32:44 +0100

    initial.

---
+++ my-manga/my-manga/my_mangaApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct my_mangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my-manga/my-mangaUITests/my_mangaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  my_mangaUITestsLaunchTests.swift
+//  my-mangaUITests
+//
+//  Created by Marlon Von Bernales Ansale on 29/05/2023.
+//
+
+import XCTest
+
+final class my_mangaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
